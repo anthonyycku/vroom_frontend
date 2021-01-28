9b7c9cc12489b9c7f21618a550eae2ad35859338
Anthony Ku <anthony.yc.ku@gmail.com>
2021-01-28 14:01:50 -0500

    added edit components

---
+++ src/CreateCompany.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class CreateCompany extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+
+}
+
+export default CreateCompany;
