ce0639f69203cdee80f0d60fb5032cf284fe2af0
Maher George Brikae <mahergeorgebrikae@Mahers-MacBook-Pro.local>
2021-01-29 11:32:48 -0500

    progress

--- src/EditCompany.js
+++ src/EditCompany.js
@@ -1,6 +1,28 @@
 import React from 'react';
 class EditCompany extends React.Component {
     
+    
+    handleChange = (event) => {
+        this.setState({
+          [event.target.id]: event.target.value,
+        })
+      } 
+      
+      handleSubmit = (event) => {
+        event.preventDefault()
+        axios.post('/companies', this.state).then((response) => {
+          this.getCompanies()
+        })
+      }
+
+      updateCompany = (event) => {
+        event.preventDefault()
+        event.target.reset()
+        const id = event.target.id
+        axios.put('/companies/' + id, this.state).then((response) => {
+          this.getCompanies()
+        })
+      }
 
 
 
