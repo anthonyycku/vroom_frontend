3205765fcacb6023e70d29bfac97bfe5041094a6
Maher George Brikae <mahergeorgebrikae@Mahers-MacBook-Pro.local>
2021-01-29 16:10:15 -0500

    edit company added

--- src/EditCompany.js
+++ src/EditCompany.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import axios from 'axios';
 
 class EditCompany extends React.Component {
+
+    state = {
+        name: '',
+        description: [],
+        country: '',
+        image: '',
+        parent_id: 0,
+      }
     
     
     handleChange = (event) => {
@@ -20,7 +29,7 @@ class EditCompany extends React.Component {
         event.preventDefault()
         event.target.reset()
         const id = event.target.id
-        axios.put('/companies/id' + id, this.state).then((response) => {
+        axios.put('/companies' + id, this.state).then((response) => {
           this.getCompanies()
         })
       }
@@ -32,21 +41,33 @@ render() {
 return (
 <div>
             <div className="company" >
-              <h4>Name: {this.props.person.name}</h4>
-              <h5>Age: {this.props.person.age}</h5>
+              <h4>Name: {this.state.company.name}</h4>
+              <h5>Discription: {this.state.company.description}</h5>
+              <h5>Country: {this.state.company.contry}</h5>
+              <h5>Image: {this.state.company.image}</h5>
+              <h5>Parent_Id: {this.state.company.parent_id}</h5>
               <details>
-                <summary>Edit Person</summary>
-                <form id={this.props.person.id} onSubmit={this.props.updatePerson}>
+                <summary>Edit Company</summary>
+                <form id={this.state.company.id} onSubmit={this.updateCompany}>
                   <label htmlFor="name">Name</label>
-                  <input type="text" id="name" onChange={this.props.handleChange} />
+                  <input type="text" id="name" onChange={this.handleChange} />
                   <br />
-                  <label htmlFor="age">Age</label>
-                  <input type="text" id="age" onChange={this.props.handleChange} />
+                  <label htmlFor="description">Discription:</label>
+                  <input type="text" id="description" onChange={this.handleChange} />
                   <br />
-                  <input type="submit" value="Update Person" />
+                  <label htmlFor="contry">Contry:</label>
+                  <input type="text" id="contry" onChange={this.handleChange} />
+                  <br />
+                  <label htmlFor="image">Image:</label>
+                  <input type="url" id="image" onChange={this.handleChange} />
+                  <br />
+                  <label htmlFor="parent_id">Parent_id:</label>
+                  <input type="text" id="parent_id" onChange={this.handleChange} />
+                  <br />
+                  <input type="submit" value="Update Company" />
                 </form>
               </details>
-              <button value={this.props.person.id} onClick={this.props.deletePerson}>
+              <button value={this.state.company.id} onClick={this.deleteCompany}>
                 X
               </button>
             </div>
