b32aa43fe57744145db5e1851cd332057315cf56
Anthony Ku <anthony.yc.ku@gmail.com>
2021-01-30 15:31:27 -0500

    added

--- src/Company.js
+++ src/Company.js
@@ -1,40 +1,82 @@
 import React from 'react';
 import './styles/styles.css'
+import axios from 'axios';
 import Nav from './Nav'
 class Company extends React.Component {
+    state = {
+        cars: [],
+        isLoaded: false
+    }
+    // componentDidUpdate = (prevProps) => {
+    //     if (this.props.company !== prevProps.company) {
+    //         this.getCars();
+    //     }
+    // }
+    componentDidMount() {
+        setTimeout(() => {
+            this.getCars();
+        }, 500)
+    }
+    getCars = () => {
+        axios.get("https://vroomies.herokuapp.com/cars/" + this.props.company.id).then(response => {
+            this.setState({
+                cars: response.data,
+                isLoaded: true
+            })
+        })
+    }
+
     render() {
         const { name, country, id, image, description } = this.props.company;
         const { gotoPage, children } = this.props;
-        return (
-            <div className="company">
-                <Nav gotoPage={gotoPage} />
-                <button onClick={() => gotoPage("main")}>Back</button>
-                <p>{name}</p>
-                <p>{country}</p>
+        const { cars } = this.state
+        if (this.state.isLoaded === true) {
+            return (
+                <div className="company">
+                    <Nav gotoPage={gotoPage} />
+                    <button onClick={() => gotoPage("main")}>Back</button>
+                    <p>{name}</p>
+                    <p>{country}</p>
 
-                <button onClick={() => gotoPage("editCompany", id)}>Edit this company</button>
+                    <button onClick={() => gotoPage("editCompany", id)}>Edit this company</button>
 
-                {/* <button onClick={() => gotoPage("createCar")}>Create</button> */}
+                    {/* <button onClick={() => gotoPage("createCar")}>Create</button> */}
 
-                <div>
+                    <div>
 
-                    {children.length > 0 ?
-                        children.map(entry => {
-                            const { childName, childID, childImage } = entry
+                        {children.length > 0 ?
+                            children.map(entry => {
+                                const { childName, childID, childImage } = entry
+                                return (
+                                    <div>
+                                        <div onClick={() => gotoPage("company", childID)}>
+                                            Name: {childName}
+                                        </div>
+                                    </div>
+                                )
+                            })
+                            :
+                            null
+                        }
+
+                        {cars.map(car => {
+                            const { model, price, rating, type, image, company_id } = car
                             return (
                                 <div>
-                                    <div onClick={() => gotoPage("company", childID)}>
-                                        Name: {childName}
-                                    </div>
+                                    <p>Model: {model}</p>
+                                    <p>Price: {price}</p>
+                                    <p>Rating: {rating}</p>
+                                    <p>Type: {type}</p>
+                                    <p>_______________________</p>
                                 </div>
                             )
-                        })
-                        :
-                        null
-                    }
+                        })}
+                    </div>
                 </div>
-            </div>
-        )
+            )
+        } else {
+            return null;
+        }
     }
 }
 
--- src/CreateCar.js
+++ src/CreateCar.js
@@ -3,82 +3,82 @@ import axios from 'axios';
 
 class CreateCar extends React.Component {
 
-    state = {
-            name: '',
-            description: [],
-            image: '',
-            country: '',
-            parent_id: 0,
-      }
+  state = {
+    name: '',
+    description: [],
+    image: '',
+    country: '',
+    parent_id: 0,
+  }
 
-      handleChange = (event) => {
-        this.setState({
-          [event.target.id]: event.target.value,
-        })
-      }
- 
-      handleSubmit = (event) => {
-        event.preventDefault()
-        axios.post("/companies", this.state).then(response => {
-          this.setState({
-            name: '',
-            description: [],
-            image: '',
-            country: '',
-            parent_id: 0,
-          })
-        })
-    
-      }
+  handleChange = (event) => {
+    this.setState({
+      [event.target.id]: event.target.value,
+    })
+  }
 
-    render() {
-    
-        return (
-            <div className="createCar">
-              <h3>Create a Car</h3>
-              <form onSubmit={this.handleSubmit}>
-              <label htmlFor="name">Name</label>
-              <input
-                type="text"
-                id="name"
-                onChange={this.handleChange}
-                value={this.state.name} />
-              <br />
-              <label htmlFor="description">Description</label>
-              <input
-                type="text"
-                id="description"
-                onChange={this.handleChange}
-                value={this.state.description} />
-              <br />
-              <label htmlFor="image">Image</label>
-              <input
-                type="text"
-                id="image"
-                onChange={this.handleChange}
-                value={this.state.image} />
-              <br />
-              <label htmlFor="country">Country</label>
-              <input
-                type="text"
-                id="country"
-                onChange={this.handleChange}
-                value={this.state.country} />
-              <br />
-              <label htmlFor="parent_id">Parent ID</label>
-              <input
-                type="text"
-                id="parent_id"
-                onChange={this.handleChange}
-                value={this.state.parent_id} />
-              <br />
-              <input
-                type="submit"
-                value="Create Car" />
-            </form>
-        </div>
-        )
-    }
+  handleSubmit = (event) => {
+    event.preventDefault()
+    axios.post("/companies", this.state).then(response => {
+      this.setState({
+        name: '',
+        description: [],
+        image: '',
+        country: '',
+        parent_id: 0,
+      })
+    })
+
+  }
+
+  render() {
+
+    return (
+      <div className="createCar">
+        <h3>Create a Car</h3>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            onChange={this.handleChange}
+            value={this.state.name} />
+          <br />
+          <label htmlFor="description">Description</label>
+          <input
+            type="text"
+            id="description"
+            onChange={this.handleChange}
+            value={this.state.description} />
+          <br />
+          <label htmlFor="image">Image</label>
+          <input
+            type="text"
+            id="image"
+            onChange={this.handleChange}
+            value={this.state.image} />
+          <br />
+          <label htmlFor="country">Country</label>
+          <input
+            type="text"
+            id="country"
+            onChange={this.handleChange}
+            value={this.state.country} />
+          <br />
+          <label htmlFor="parent_id">Parent ID</label>
+          <input
+            type="text"
+            id="parent_id"
+            onChange={this.handleChange}
+            value={this.state.parent_id} />
+          <br />
+          <input
+            type="submit"
+            value="Create Car" />
+        </form>
+      </div>
+    )
+  }
 }
 
 export default CreateCar;
