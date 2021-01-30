913674b67f454c4345fbed3c475930eb9a7eb21e
Andrett Duvenhage <andrettiduv@gmail.com>
2021-01-30 12:54:14 -0800

    css

--- src/Company.js
+++ src/Company.js
@@ -3,19 +3,22 @@ import './styles/styles.css'
 import Nav from './Nav'
 class Company extends React.Component {
     render() {
-        const { name, country, id } = this.props.company;
+        const { name, country, id, image, description } = this.props.company;
         const { gotoPage, children } = this.props;
         return (
-            <div>
+            <div className="ContainerCompany">
                 <Nav gotoPage={gotoPage} />
-                <button onClick={() => gotoPage("main")}>Back</button>
-                <p>{name}</p>
-                <p>{country}</p>
-                <button onClick={() => gotoPage("editCompany", id)}>Edit this company</button>
+                <button class="myButton" onClick={() => gotoPage("main")}>Back</button>
+                <img className="companyImage" src={image}></img>
+                <p className="companyName">{name}</p>
+                <p className="companyCountry">{country}</p>
+                <p className="companyDescription">{description} description </p>
+                
+                <button class="myButton" onClick={() => gotoPage("editCompany", id)}>Edit this company</button>
 
                 {/* <button onClick={() => gotoPage("createCar")}>Create</button> */}
 
-                <div>
+                <div className="childCompanies">
 
                     {children.length > 0 ?
                         children.map(entry => {
@@ -23,7 +26,8 @@ class Company extends React.Component {
                             return (
                                 <div>
                                     <div onClick={() => gotoPage("company", childID)}>
-                                        Name: {childName}
+                                         {childName} 
+                                         <img className="childImage" src={childImage}></img>{}
                                     </div>
                                 </div>
                             )
